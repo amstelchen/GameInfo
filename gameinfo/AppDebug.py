aaaab337417a8fdddbf1efd4960493ac31599252
Michael John <michael.john@gmx.at>
2022-06-08 20:18:07 +0200

    Improve debugging

--- gameinfo/AppDebug.py
+++ gameinfo/AppDebug.py
@@ -1,5 +1,6 @@
 from .__init__ import *
 from .GameInfo import *
+from datetime import datetime
 
 gettext = gettext.translation('gameinfo', localedir=os.path.join(os.path.dirname(__file__), 'locales'))
 gettext.install("gameinfo")
@@ -8,9 +9,14 @@ _ = gettext.gettext
 WaitMessage = _("Fetching system info, this can take a second...")
 
 class AppDebug():
-    def debug_print(message):
+    def debug_print(message: str, prefix="DEBUG: ", end="\n", show_time = True):
         try:
             if os.environ['GAMEINFO_DEBUG'] == "1":
-                print("DEBUG: " + message)
-        except:
+                if show_time:
+                    date_str = str(datetime.now())
+                    timedate = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S.%f')
+                else:
+                    timedate = ""
+                print(f"{timedate} {prefix}{message}", end=end, flush=True)
+        except KeyError:
             pass
